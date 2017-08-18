ea05b64d668ca7266f8fb5c4b19bb5858149d5f9
Andauquer <geba23@hotmail.com>
2017-08-18 01:32:06 +0000

    Update views and navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  # Modificamos esta linea para hacerle saber a la ruta que el controlador de registrations
+  # debe tomar en cuenta el archivo que creamos en users/registrations
+  devise_for :users, :controllers => { :registrations => "user/registrations" }
   resources :user_stocks, except: [:show, :edit, :update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
